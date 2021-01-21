49210a6489a0e97da33858ed1af43e3e57eb10f6
csouder <77806064+csouder@users.noreply.github.com>
2021-01-21 15:48:45 -0500

    Add files via upload

---
+++ CombineStrings/CombineStrings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+
+namespace CombineStrings
+{
+    public class Combiner
+    {
+        private string[] fragments;
+        private char divider;
+
+        public Combiner()
+        {
+            this.divider = '-';
+            this.fragments = new string[] {"This","is","default"};
+        }
+        public Combiner(char divider, string[] fragments)
+        {
+            this.divider = divider;
+            this.fragments = fragments;
+        }
+
+        //Returns the combination of string fragments with the dividers in place
+        public string GetCombination()
+        {
+            string combo = "";
+            foreach (string part in this.fragments)
+            {
+                combo += part + this.divider;
+            }
+            string final = combo.Remove(combo.Length - 1); // Remove divider at end from last loop iteration
+            return final;
+        }
+
+        /// <summary>
+        /// Combines/joins the supplied <paramref name="fragments"/> and ensures
+        /// only one instance of <paramref name="separator"/> between them.
+        /// Essentially a <see cref="System.IO.Path.Combine(string[])"/> that
+        /// accepts an argument for the separator.
+        /// </summary>
+        public static string Combine(char separator, params string[] fragments)
+        {
+            if (string.IsNullOrEmpty(separator.ToString())
+                || separator == '\0')
+            {
+                throw new ArgumentNullException("separator");
+            }
+            else if (fragments == null || fragments.Contains(""))
+            {
+                throw new ArgumentNullException("fragments");
+            }
+            var tester = new Combiner(separator, fragments);
+            string fullstring = "";
+            try
+            {
+                fullstring = tester.GetCombination();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return fullstring;
+        }
+    }
+}
---
+++ CombineStringsTest/CombineStringsTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CombineStrings;
+
+namespace CombineStringsTest {
+
+    [TestClass]
+    public class CombineStringsTest
+    {
+        const char separator = ',';
+
+
+        //Test main function with correct input
+        [TestMethod]
+        public void Maintest()
+        {
+            string[] fragments = new string[] { "Main", "test", "here" };
+            string expected = "Main,test,here";
+            string tester = CombineStrings.Combiner.Combine(separator, fragments);
+            Assert.AreEqual(expected, tester);
+        }
+
+        //Test default constructor
+        [TestMethod]
+        public void defaulttest()
+        {
+            Combiner defaulttest = new Combiner();
+            string finalstring = defaulttest.GetCombination();
+            Assert.AreEqual("This-is-default", finalstring);
+        }
+
+        [TestMethod]
+        [Owner("Chase")]
+        [TestCategory("NULLs")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void separatornull()
+        {
+            char separator = '\0';
+            string[] fragments = new string[] { "Dont", "pass", "this" };
+            string tester = CombineStrings.Combiner.Combine(separator, fragments);
+            Assert.Fail("Exception was not thrown");
+        }
+
+        [TestMethod]
+        [Owner("Chase")]
+        [TestCategory("NULLs")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void listofstringsnull()
+        {
+            string[] fragments = null;
+            string tester = CombineStrings.Combiner.Combine(separator, fragments);
+            Assert.Fail("Exception was not thrown");
+        }
+    }
+}
